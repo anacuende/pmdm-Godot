748498b35bb0b968c0cbbe35ebf0506879a8b77c
anacuende <acuendet@fpcoruna.afundacion.org>
2023-12-08 21:19:11 +0100

    Añadido mob y tilemaps terminados

--- Juego/Juego/Ducky.gd
+++ Juego/Juego/Ducky.gd
@@ -6,26 +6,27 @@ var player
 var chase = false
 
 func _ready():
-	get_node("AnimatedSprite2D").play("Run")
+	get_node("AnimatedSprite2D").play("Idle")
 
 func _physics_process(delta):
-	# Gravedad para la rana
+	#Gravity para el pesao del patico :/
 	velocity.y += gravity * delta
 	if chase == true:
 		if get_node("AnimatedSprite2D").animation != "Death":
 			get_node("AnimatedSprite2D").play("Run")
 		player = get_node("../../Player/Player")
 		var direction = (player.position - self.position).normalized()
-		if direction.x > 0:
-			get_node("AnimatedSprite2D").flip_h = true
-		else:
+		if direction.x < 0:
 			get_node("AnimatedSprite2D").flip_h = false
-		velocity.x = direction.x * SPEED
+		else:
+			get_node("AnimatedSprite2D").flip_h = true
+		velocity.x = -direction.x * SPEED
 	else:
 		if get_node("AnimatedSprite2D").animation != "Death":
-			get_node("AnimatedSprite2D").play("Run")
+			get_node("AnimatedSprite2D").play("Idle")
 		velocity.x = 0
 	move_and_slide()
+
 func _on_player_detection_body_entered(body):
 	if body.name == "Player":
 		chase = true
